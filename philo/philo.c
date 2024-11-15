19e601854207da17720575ce8120f5d70ec91254
MoniNog <nogueiramon@gmail.com>
2024-11-15 12:49:31 +0100

    fin du cours 1

--- philo/philo.c
+++ philo/philo.c
@@ -6,13 +6,38 @@
 /*   By: monoguei <monoguei@lausanne42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 11:27:34 by monoguei          #+#    #+#             */
-/*   Updated: 2024/11/15 11:27:41 by monoguei         ###   ########.fr       */
+/*   Updated: 2024/11/15 12:19:00 by monoguei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main() {
-	printf("Hello, World!\n");
+void	parsing(t_param **param, char **av)
+{
+		*param = malloc(sizeof(t_param));
+	
+		
+	(*param)->nb_philo = atoi(av[1]);
+	(*param)->t_die = atoi(av[2]);
+	(*param)->t_eat = atoi(av[3]);
+	(*param)->t_sleep = atoi(av[4]);
+	(*param)->t_each_philo_eat = atoi(av[5]);
+
+	(*param)->philos = malloc(sizeof(t_philo) * (*param)->nb_philo);
+}
+
+int main(int ac, char **av)
+{
+	t_param	*param;
+
+	param = NULL;
+	if (ac == 6)
+		parsing(&param, av);
+	
+    printf("nb philo : %i\n", param->nb_philo);
+	printf("time to die : %i\n", param->t_die);
+	printf("time to eat : %i\n", param->t_eat);
+	printf("time to sleep : %i\n", param->t_sleep);
+	printf("times each philo eats : %i\n", param->t_each_philo_eat);
 	return 0;
 }
--- philo/philo.h
+++ philo/philo.h
@@ -2,5 +2,33 @@
 # define PHILO_H
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef	struct s_philos
+{
+	int		id_philo;// numero du philo
+	int		activity; //1 think, 2 eat, 3 sleep, 0 die
+
+}				t_philo;
+
+typedef struct s_param
+{
+	int		nb_philo;
+	t_philo	*philos;
+	int		t_die;
+	int		t_eat;
+	int		t_sleep;
+	int		t_each_philo_eat;
+}			t_param;
+
+
+
+
+// typedef struct s_thread;
+// {
+// 	pthread_t	thread;
+// 	int			etat;
+// 	pmutex_t	mutexl	
+// }				t_thread;
 
 #endif
